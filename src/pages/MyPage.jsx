b97e31f75b703b78023bea2d2977ace3406d4a2f
DongHyun <pca468@naver.com>
2024-12-04 22:10:43 +0900

    mypage add

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,58 +1,56 @@
-import React, { useRef, useState, useEffect } from 'react';
-import left_icon from '../ms_image/left_icon.png';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef, useState, useEffect } from "react";
+import left_icon from "../ms_image/left_icon.png";
+import { useNavigate } from "react-router-dom";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import "../css/MyPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MyPage = () => {
-
   const navigate = useNavigate();
 
   const handleLeftButtonClick = () => {
     navigate(-1);
-  }
+  };
 
   return (
-    <div style={styles.container}>
-      <div style={styles.titleContainer}>
-                <button style={styles.leftButton} onClick={handleLeftButtonClick}></button>
-                <div style={styles.title}>마이페이지</div>
-            </div>
+    <div className="mypage-container">
+      {/* profile section */}
+      <div className="background1">
+        <div className="title-container">
+          <button
+            className="left-button"
+            onClick={handleLeftButtonClick}
+          ></button>
+          <div className="title">마이페이지</div>
+        </div>
+        <div className="profile-container">
+          <div className="img-container">
+            <FontAwesomeIcon icon={faUser} className="icon-style" />
+          </div>
+          <div className="profile-name">
+            <h2>홍길동</h2>
+          </div>
+        </div>
+      </div>
+      {/* info section */}
+      <div className="info-container">
+        <div className="info-row">
+          <div className="alert-count-container">
+            <h3>경고음 횟수 :</h3>
+            <h2>2회</h2>
+          </div>
+          <div className="drive-status-container">
+            <h3>운전 상태 : </h3>
+            <h2>😄</h2>
+          </div>
+        </div>
+        <div className="notAlert-day-container">
+          <h3>안전운전 일수 :</h3>
+          <h2>연속 7일</h2>
+        </div>
+      </div>
     </div>
-  )
-}
-const styles = {
-  container: {
-      display: 'flex',
-      width: '100%',
-      height: '100vh', 
-      flexDirection: 'column', 
-      alignItems: 'center',
-      backgroundColor: '#EAEAEA',
-  },
-  titleContainer: {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      // justifyContent: 'space-between',
-      width: '100%',
-      
-  },
-  leftButton: {
-      width: '40px',
-      height: '40px',
-      border: 'none',
-      backgroundImage: `url(${left_icon})`,
-      backgroundPosition: 'center', 
-      backgroundSize: 'cover',
-      backgroundColor: '#EAEAEA',
-      marginLeft: '20px',
-  },
-  title: {
-    marginTop: '10px',
-    textAlign: 'center',
-    fontSize: '24px',
-    marginBottom: '20px',
-    marginLeft: '70px',
-},
-}
+  );
+};
 
-export default MyPage
+export default MyPage;
